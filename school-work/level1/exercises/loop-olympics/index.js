08341f59626ff53982d37f6ddcaa6750e8bfefbc
MaKayla Mihu <>
2024-02-01 18:49:27 -0700

    loops and arrays stuff

---
+++ school-work/level1/exercises/loop-olympics/index.js
@@ -0,0 +1,22 @@
+//Preliminaries
+    //Write a for loop that prints to the console the numbers 0 trough 9.
+
+
+    //Write a for loop that prints to the console 9 through 0.
+
+    
+    //Write a for loop that prints these fruits to the console
+const fruit = ["banana", "orange", "apple", "kiwi"]
+
+
+
+//Bronze Medal
+    //write a fro loop that will push the numbers 0 through 9 to an array.
+
+
+    //Write a for loop that prints to the console only even numbers 0 through 100
+
+    //Write a for loop that will push every other fruit to an array
+const fruits = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+
+
---
+++ school-work/level1/exercises/loops-and-arrays-practice/index.js
@@ -0,0 +1,47 @@
+var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer",  "computer"];
+
+
+for(i = 0; i < officeItems.length; i++){
+    var computers = 0
+    if(i = "computer"){
+        computers++
+    }
+    if(i = officeItems.length - 1){
+        console.log(computers)
+    }
+    return computers
+};
+
+
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+    {
+      name: "Mike",
+      age: 12,
+      gender: "male"
+    },{
+      name: "Madeline",
+      age: 80,
+      gender: "female"
+    },{
+      name: "Cheryl",
+      age: 22,
+      gender: "female"
+    },{
+      name: "Sam",
+      age: 30,
+      gender: "male"
+    },{
+      name: "Suzy",
+      age: 4,
+      gender: "female"
+    }
+  ]
+ 
+  
+// for(i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++){
+//     if(peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18){
+//         console.log("old enough")
+//     } else{
+//         console.log("not old enough")
+//     }
+// }
