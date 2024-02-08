13321f52e650ce88cefb4946c706d1ab16f536c4
MaKayla Mihu <>
2024-02-08 13:56:54 -0700

    finishing up lessons before meeting 2

---
+++ school-work/level1/assignments/escape-room/index.js
@@ -0,0 +1,10 @@
+const readline = require("readline-sync")
+//const name = readline.question("What is your name? ")
+
+/*Write a game that lets a user escape a room. The character finds him/herself locked in a room. In order to escape the room, your character needs to find the key in the room and then open the door. There's also a hole in the wall in the room. If your character puts his hand in the hole, he dies.
+    **Game start:**
+Tell the user their options:
+    1. Find the key, or
+    2. Put hand in hole
+    3. Open the door
+They can't open the door unless they find the key first.They die if they put their hand in the hole. */
