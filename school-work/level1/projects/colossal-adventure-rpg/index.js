9ec9c816365adf13ad7accdc87aa128bc22f5874
MaKayla Mihu <>
2024-02-19 15:34:51 -0700

    a couple exercises and starting level 1 capstone

---
+++ school-work/level1/projects/colossal-adventure-rpg/index.js
@@ -0,0 +1,9 @@
+const readline = require("readline-sync");
+
+
+const name = readline.question("Welcome to the Colossal Adventure RPG! \nWhat is your name? ")
+
+//fuction one while loop for walking
+//let variable to note if enemy has appeared or not (nmber changes based on random 3 options)
+//function for the different monsters (dependent on number of let variable)
+//object for inventory
